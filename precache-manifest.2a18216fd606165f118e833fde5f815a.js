7f37cb7ce1699f817c45315d088deeac8b44d453
Simon Pettersson <simon.pettersson@protonmail.com>
2019-10-28 09:58:39 +0100

    Updates

---
+++ precache-manifest.2a18216fd606165f118e833fde5f815a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c907e61691947edddf4b6977dca763ab",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "764babacaab525a0a71c",
+    "url": "/monsters-rolodex/static/css/main.adf29c91.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "764babacaab525a0a71c",
+    "url": "/monsters-rolodex/static/js/main.75166eb1.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
